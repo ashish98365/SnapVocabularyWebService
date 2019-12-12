dc1149202a2a0ecd8a42166a7a5a3358c7c31bf4
ashish <ashish@localhost.localdomain>
2019-12-12 06:35:08 -0500

    specifying correct path for jar

--- mvn.sh
+++ mvn.sh
@@ -9,14 +9,15 @@ WORKSPACE=/home/ashish/jenkins-data/jenkins_home/workspace/pipe-job
 #Creating Jar File
 docker run --rm -v $PWD:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
 
-#Creating Image
-docker build -t snapvocabularyservice -f  src/main/docker/Dockerfile .
 
 #Deleting container if their is any
 docker stop snapvocabularyservice
 docker rm snapvocabularyservice
 
-#Creating COntainer
+#Creating Image
+docker build -t snapvocabularyservice -f  src/main/docker/Dockerfile .
+
+#Creating Container
 docker run --name snapvocabularyservice  -d -p 8081:8080 snapvocabularyservice
 
 #docker build -t test:latest -f setup/docker/Dockerfile .
