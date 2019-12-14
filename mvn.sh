a5972f152f60b133f15e48b474826b3cd24927bf
ashish98365 <ashish98365@gmail.com>
2019-12-14 10:36:55 +0530

    workspace folder path is change according to laptop path

--- mvn.sh
+++ mvn.sh
@@ -4,10 +4,10 @@ echo "*********************************"
 echo "****** Building image ***********"
 echo "*********************************"
 
-WORKSPACE=/home/ashish/jenkins-data/jenkins_home/workspace/pipe-job
+WORKSPACE=/home/ashish/ashish/work/learning/jenkins/jenkins_home/workspace
 
 #Creating Jar File
-docker run --rm -v /home/ashish/jenkins-data/jenkins_home/workspace/snap-vocabulary-service/:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/snap-vocabulary-service/:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
 
 
 #Deleting container if their is any
@@ -22,8 +22,3 @@ docker build -t snapvocabularyservice -f  src/main/docker/Dockerfile .
 
 #Creating Container
 docker run --name snapvocabularyservice  -d -p 8081:8080 snapvocabularyservice
-
-#docker build -t test:latest -f setup/docker/Dockerfile .
-
-#docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
-
